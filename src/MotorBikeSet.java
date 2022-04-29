176910667396e9f40d19f187aab76e44f8d53f25
marc.nebot.moyano <marc.nebot.moyano@estudiantat.upc.edu>
2022-04-30 01:06:35 +0200

    asd

--- src/MotorBikeSet.java
+++ src/MotorBikeSet.java
@@ -1,10 +1,15 @@
 import java.util.HashMap;
 
 public class MotorBikeSet {
-    private static HashMap<Integer, MotorBike> motorBikeSet;
+    private static HashMap<Integer, MotorBike> motorBikeSet = new HashMap<>();
 
     public MotorBike getMotorBike(int id){
         return motorBikeSet.get(id);
     }
     public static HashMap<Integer, MotorBike> getMotorBikes(){return motorBikeSet;}
+
+    public void addMotorBike(int id, String name, int brandID, int year, int fuel, int price){
+        MotorBike bike = new MotorBike(id, name, brandID, year, fuel, price);
+        motorBikeSet.put(id, bike);
+    }
 }
