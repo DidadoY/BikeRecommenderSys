c31958f5d218d527475840afac0aa22f0ca7f2ea
marc.camarillas <marc.camarillas@estudiantat.upc.edu>
2022-04-30 01:25:27 +0200

    "class motorbike"

--- src/MotorBikeSet.java
+++ src/MotorBikeSet.java
@@ -7,7 +7,14 @@ public class MotorBikeSet {
     public MotorBike getMotorBike(int id){
         return motorBikeSet.get(id);
     }
-    public static HashMap<Integer, MotorBike> getMotorBikes(){return motorBikeSet;}
+    public static HashMap<Integer, MotorBike> getMotorBikes(){
+        return motorBikeSet;
+    }
+
+    public void addMotorBike(int id, String name, int brandID, int year, int fuel, int price){
+        MotorBike bike = new MotorBike(id, name, brandID, year, fuel, price);
+        motorBikeSet.put(id, bike);
+    }
     public void printMotorBikeSet(){
         System.out.println("Printing all the MotorBikes...");
         for (Map.Entry<Integer, MotorBike> entry : motorBikeSet.entrySet()) {
