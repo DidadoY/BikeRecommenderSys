9d5ede8b8d1ace6d77ca66b2547caa091fe47447
alejandro.salvat.navarro <alejandro.salvat.navarro@estudiantat.upc.edu>
2022-04-30 00:26:37 +0200

    a

--- src/brandSet.java
+++ src/brandSet.java
@@ -1,2 +1,14 @@
-public class brandSet {
+
+import java.util.HashMap;
+
+public class brandSet<hashMap> {
+    public HashMap<Integer, String> brandSet;
+
+    public brandSet(){
+        this.brandSet = new HashMap<>();
+    }
+
+    public String getName(Integer id){
+        return brandSet.get(id);
+    }
 }
